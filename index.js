d2ee66e54b0e7b7010ff37d9f4c4c2a007167347
kamwaro <petersonkamwaro@gmail.com>
2021-01-28 03:49:12 +0300

    learnt about post request in express

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const logger = require('./mIddleware/logger')
-const members = require('./Members')
+const logger = require('./mIddleware/logger'); 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -9,16 +8,17 @@ const app = express();
 
 //Init logger middleware
 app.use(logger);
-// Gets all members
-app.get('/api/members', (req,res) => res.json(members))
-// Get one member
-app.get('/api/members/:id', (req, res) => {
-    res.json(members.filter(member => member.id == parseInt(req.params.id)));
-})
 
+// Body Parser Middleware
+app.use(express.json());
+// Handle form submissions
+app.use(express.urlencoded({extended: false}))
 
 // Set a static folder
 app.use(express.static(path.join(__dirname,"public")));
+
+// Set members router
+app.use('/api/members',require('./routes/api/members'))
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const uuid = require('uuid');
 const router = express.Router();
 const members = require('../../Members')
 
@@ -10,6 +11,23 @@ router.get('/:id', (req, res) => {
     
     found ? res.json(members.filter(member => member.id == parseInt(req.params.id))) : res.status(400).json({msg:`No member with id ${req.params.id}`})
     })
+
+// Create Member
+router.post('/', (req, res) => {
+   const newMember = {
+       id: uuid.v4(),
+       name:req.body.name,
+       email:req.body.email,
+       status:'active'
+   }
+
+   if(!newMember.name || !newMember.email){
+     return res.status(400).json({msg:"Please include a name and email."})
+   }
+
+   members.push(newMember);
+   res.json(members);
+})
 
 
 module.exports = router;
